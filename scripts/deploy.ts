3ad16e584da009b9f9e1a76ee06b3f3f3e643515
albert.zhang <shellteo@163.com>
2023-03-26 21:28:48 +0800

    just depoly HexNFT script

---
+++ scripts/deploy.ts
@@ -0,0 +1,24 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+// When running the script with `hardhat run <script>` you'll find the Hardhat
+// Runtime Environment's members available in the global scope.
+import { ethers } from 'hardhat';
+import { Contract, ContractFactory } from 'ethers';
+
+async function main(): Promise<void> {
+  const HexNFT: ContractFactory = await ethers.getContractFactory(
+    'HexNFT',
+  );
+  const hexNFT: Contract = await HexNFT.deploy();
+  await hexNFT.deployed();
+  console.log('TestToken deployed to: ', hexNFT.address);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error: Error) => {
+    console.error(error);
+    process.exit(1);
+  });
